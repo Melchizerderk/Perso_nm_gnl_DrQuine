5120885b9609a46d980fd9ad894b9a12da03e8c1
Baptiste CRESPIN <bcrespin@e1r11p4.42.fr>
2016-04-27 17:56:13 +0200

    debut grace

---
+++ DrQuine/Grace.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define QT 34
+#define NL 10
+#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c", r+); char a[]="#include <stdio.h>%c#define QT 34%c#define NL 10%c#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c, r+"); char a[]=%c%s%c;fd = printf(a,Nl,NL,NL,QT,a,NL,NL,NL,NL,9,NL,NL);%c%cFT(xxxxxxxx)%c/*%c%cUn commentaire%c%c";fd = printf(a,NL,NL,NL,QT,a,NL,NL,NL,NL,9,NL,NL);}
+
+FT(xxxxxxxx)
+/*
+ 	Un commentaire
+*/
