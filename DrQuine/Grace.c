690c30b5fc4a1cdfef549e902286be107336257c
Baptiste CRESPIN <bcrespin@e1r11p4.42.fr>
2016-04-28 17:29:59 +0200

    grace segfault

--- DrQuine/Grace.c
+++ DrQuine/Grace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define QT 34
 #define NL 10
-#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c", r+); char a[]="#include <stdio.h>%c#define QT 34%c#define NL 10%c#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c, r+"); char a[]=%c%s%c;fd = printf(a,Nl,NL,NL,QT,a,NL,NL,NL,NL,9,NL,NL);%c%cFT(xxxxxxxx)%c/*%c%cUn commentaire%c%c";fd = printf(a,NL,NL,NL,QT,a,NL,NL,NL,NL,9,NL,NL);}
+#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c", "r+"); char a[]="#include <stdio.h>%c#define QT 34%c#define NL 10%c#define FT(x)int main(){FILE *fd = fopen(%cGrace_kid.c%c, %cr+%c); char a[]=%c%s%c;fprintf(fd,a,Nl,NL,NL,QT,QT,QT,QT,QT,a,NL,NL,NL,NL,9,NL,NL);%c%cFT(xxxxxxxx)%c/*%c%cUn commentaire%c%c";fprintf(fd,a,NL,NL,NL,QT,QT,QT,QT,QT,a,NL,NL,NL,NL,9,NL,NL);}
 
 FT(xxxxxxxx)
 /*
