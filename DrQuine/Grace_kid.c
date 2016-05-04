6e7ec6d01bd506269a1b0287007bb246f6a30625
Baptiste CRESPIN <bcrespin@e1r11p4.42.fr>
2016-05-04 10:45:08 +0200

    colleen and grace ok

--- DrQuine/Grace_kid.c
+++ DrQuine/Grace_kid.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #define QT 34
 #define NL 10
-#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c", "w+"); char a[]="#include <stdio.h>%c#define QT 34%c#define NL 10%c#define FT(x)int main(){FILE *fd = fopen(%cGrace_kid.c%c, %cw+%c); char a[]=%c%s%c;fprintf(fd,a,NL,NL,NL,QT,QT,QT,QT,QT,a,NL,NL,NL,NL,9,NL,NL);%c%cFT(xxxxxxxx)%c/*%c%cUn commentaire%c*/%c
-;fprintf(fd,a,NL,NL,NL,QT,QT,QT,QT,QT,a,NL,NL,NL,NL,9,NL,NL);
+#define FT(x)int main(){FILE *fd = fopen("Grace_kid.c", "w+"); char a[]="#include <stdio.h>%c#define QT 34%c#define NL 10%c#define FT(x)int main(){FILE *fd = fopen(%cGrace_kid.c%c, %cw+%c); char a[]=%c%s%c;fprintf(fd,a,NL,NL,NL,QT,QT,QT,QT,QT,a,QT,NL,NL,NL,NL,9,NL,NL);}%c%cFT(xxxxxxxx)%c/*%c%cUn commentaire%c*/%c";fprintf(fd,a,NL,NL,NL,QT,QT,QT,QT,QT,a,QT,NL,NL,NL,NL,9,NL,NL);}
 
 FT(xxxxxxxx)
-/*	
-Un commentaire
-*/
+/*
+	Un commentaire
+*/
