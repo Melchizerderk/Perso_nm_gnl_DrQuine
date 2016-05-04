31aa48ef51dc8195a7599a08bb1622ff13f755f5
Baptiste CRESPIN <bcrespin@e1r11p4.42.fr>
2016-05-04 17:07:38 +0200

    sully main fais chier

--- DrQuine/Sully_5.c
+++ DrQuine/Sully_5.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 int main(){
-char buf[10];char cmd[255];
+char buf[10];char cmd[255];char cname[9];
 int i = 4;
 if (i >= 0){
-snprintf(buf, sizeof(buf), "Sully_%i.c", i);i--;
-strcpy(cmd, "/usr/bin/clang -Wall -Werror -Wextra ");strcat(cmd, buf);
-FILE *fd = fopen(buf, "w+");char a[]="#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%cint main(){%cchar buf[10];char cmd[255];%cint i = %i;%cif (i >= 0){%csnprintf(buf, sizeof(buf), %cSully_%ci.c%c, i);i--;%cstrcpy(cmd, %c/usr/bin/clang -Wall -Werror -Wextra %c);strcat(cmd, buf);%cFILE *fd = fopen(buf, %cw+%c);char a[]=%c%s%c;fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,10,34,34,10,34,34,34,a,34,34,34,10);}system(cmd);system(%c./a.out%c);}%c";fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,10,34,34,10,34,34,34,a,34,34,34,10);}system(cmd);system("./a.out");}
+snprintf(cname, sizeof(cname), "Sully_%i ");snprintf(buf, sizeof(buf), "Sully_%i.c", i);i--;
+strcpy(cmd, "/usr/bin/clang -Wall -Werror -Wextra ");strcat(cmd, cname);strcat(cmd, buf);
+FILE *fd = fopen(buf, "w+");char a[]="#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%cint main(){%cchar buf[10];char cmd[255];char cname[9];%cint i = %i;%cif (i >= 0){%csnprintf(cname, sizeof(cname), %cSully_%ci %c);snprintf(buf, sizeof(buf), %cSully_%ci.c%c, i);i--;%cstrcpy(cmd, %c/usr/bin/clang -Wall -Werror -Wextra %c);strcat(cmd, cname);strcat(cmd, buf);%cFILE *fd = fopen(buf, %cw+%c);char a[]=%c%s%c;fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,34,37,34,10,34,34,10,34,34,34,a,34,10);}system(cmd);system(cname);}%c";fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,34,37,34,10,34,34,10,34,34,34,a,34,10);}system(cmd);system(cname);}
