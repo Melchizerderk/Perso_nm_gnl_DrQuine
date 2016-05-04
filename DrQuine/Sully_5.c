76a3e1789100bbd87ac852db9b490155dda0dadd
Baptiste CRESPIN <bcrespin@e1r11p4.42.fr>
2016-05-04 16:07:29 +0200

    Sully presque bon, pb de compil

--- DrQuine/Sully_5.c
+++ DrQuine/Sully_5.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 int main(){
-char buf[10];
+char buf[10];char cmd[255];
 int i = 4;
 if (i >= 0){
 snprintf(buf, sizeof(buf), "Sully_%i.c", i);i--;
-FILE *fd = fopen(buf, "w+");char a[]="#include <stdio.h>%cint main(){%cchar buf[10];%cint i = %i;%cif (i >= 0){%csnprintf(buf, sizeof(buf), %cSully_%ci.c%c, i);i--;%cFILE *fd = fopen(buf, %cw+%c);char a[]=%c%s%c;fprintf(fd,a,10,10,10,i,10,10,34,37,34,10,34,34,34,a,34,10);}}%c";fprintf(fd,a,10,10,10,i,10,10,34,37,34,10,34,34,34,a,34,10);}}
+strcpy(cmd, "/usr/bin/clang -Wall -Werror -Wextra ");strcat(cmd, buf);
+FILE *fd = fopen(buf, "w+");char a[]="#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%cint main(){%cchar buf[10];char cmd[255];%cint i = %i;%cif (i >= 0){%csnprintf(buf, sizeof(buf), %cSully_%ci.c%c, i);i--;%cstrcpy(cmd, %c/usr/bin/clang -Wall -Werror -Wextra %c);strcat(cmd, buf);%cFILE *fd = fopen(buf, %cw+%c);char a[]=%c%s%c;fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,10,34,34,10,34,34,34,a,34,34,34,10);}system(cmd);system(%c./a.out%c);}%c";fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,10,34,34,10,34,34,34,a,34,34,34,10);}system(cmd);system("./a.out");}
