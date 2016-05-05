492fd7ab2a2f9be6c351dff18f26975a9ea0be9a
Baptiste CRESPIN <bcrespin@e1r11p2.42.fr>
2016-05-05 15:06:16 +0200

    ca marche a l exterieur mais pas dans le main, pe sepparer en 2 func?

--- DrQuine/Sully.c
+++ DrQuine/Sully.c
@@ -8,4 +8,5 @@ int i = 5;
 if (i >= 0){
 snprintf(buf, sizeof(buf), "Sully_%i.c", i);i--;
 strcpy(cmd, "/usr/bin/clang -Wall -Werror -Wextra ");strcat(cmd, buf);
+printf("%s", cmd);
 FILE *fd = fopen(buf, "w+");char a[]="#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%cint main(){%cchar buf[10];char cmd[255];%cint i = %i;%cif (i >= 0){%csnprintf(buf, sizeof(buf), %cSully_%ci.c%c, i);i--;%cstrcpy(cmd, %c/usr/bin/clang -Wall -Werror -Wextra %c);strcat(cmd, buf);%cFILE *fd = fopen(buf, %cw+%c);char a[]=%c%s%c;fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,10,34,34,10,34,34,34,a,34,34,34,10);}system(cmd);system(%c./a.out%c);}%c";fprintf(fd,a,10,10,10,10,10,10,i,10,10,34,37,34,10,34,34,10,34,34,34,a,34,34,34,10);}system(cmd);system("./a.out");}
