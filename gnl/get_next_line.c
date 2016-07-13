ebab6a4732d293b8f3b000981765d1da43845a34
Baptiste CRESPIN <bcrespin@e1r2p5.42.fr>
2016-07-13 17:27:43 +0200

    juste a gerer le fichier vide

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,17 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcrespin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/13 14:48:36 by bcrespin          #+#    #+#             */
+/*   Updated: 2016/07/13 17:05:41 by bcrespin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char *ft_keepreading(const int fd, int *i)
+int ft_findchar(char *str, char c)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return (i + 1);
+      i++;
+	}
+	return (-1);
+}
+
+char *ft_keepreading(const int fd, int *i, int **readreturn)
 {
 	char *temp;
 	char *buf;
 
 	temp = malloc(sizeof(char) * BUFF_SIZE);
 	buf = malloc(sizeof(char) * BUFF_SIZE);
-	read(fd, buf, BUFF_SIZE);
+	**readreturn = read(fd, buf, BUFF_SIZE);
 	*i = ft_findchar(buf, '\n');
 	while (*i == -1)
 	{
-		read(fd, temp, BUFF_SIZE);
+		**readreturn = read(fd, temp, BUFF_SIZE);
 		buf = ft_strdup(ft_strjoin(buf, temp));
 		*i = ft_findchar(buf, '\n');
 	}
@@ -23,8 +48,7 @@ char *ft_keepreading(const int fd, int *i)
 char *ft_tosaveornot(char **buf, int i, char *savedread)
 {
 	int d;
-	char **tab;
-	
+
 	if (i == ft_strlen(*buf))
 	{
 		*buf = ft_strsub(*buf, 0, ft_strlen(*buf) - 1); /*pour enlever le backslash n*/
@@ -32,72 +56,53 @@ char *ft_tosaveornot(char **buf, int i, char *savedread)
 	}
 	if (i <	ft_strlen(*buf))
 	{
-		printf("OH FDP\n");
-	/*	tab = ft_strsplit(*buf, '\n');
-		while(tab[i] != '\0')
-		{
-			printf("%s\n", tab[i]);
-			i++;
-		}*/
-		savedread = ft_strdup(*buf);
-	/*	d = ft_strlen(savedread) - (BUFF_SIZE - i);
-		printf("%d\n", d);*/
-	/*	if (d < 0)
-		{*/
-			savedread = ft_strsub(savedread, i, ft_strlen(savedread));
-			*buf = ft_strsub(*buf, 0, i);
-	//		printf("ICI SOUCI %d : %s : %s\n",i, *buf, savedread);
-	/*	}
+		if (savedread)
+			d = ft_strlen(savedread) + i;
 		else
-		{
-			printf("ICI FDP\n");
-			savedread = ft_strsub(savedread, \
-			ft_strlen(savedread) - (BUFF_SIZE - i), BUFF_SIZE - i);
-			printf("%s : %s\n", *buf, savedread);
-			*buf = ft_strsub(*buf, 0, ft_strlen(*buf) - (BUFF_SIZE - i - 1));
-		}*/
+			d = i;
+		savedread = ft_strdup(*buf);
+		savedread = ft_strsub(savedread, d, ft_strlen(savedread));
+		*buf = ft_strsub(*buf, 0, d - 1);
 		return (savedread);
 	}
+	return (NULL);
 }
 
-char *ft_read(const int fd, char **savedread, int readstatus)
+char *ft_read(const int fd, char **savedread, int readstatus, int *readreturn)
 {
 	char	*buf;
 	int	i;
 	int d;
 
-	if (readstatus != -1)
+	if (readstatus == 0)
 	{
-		if (readstatus == 0)
+		buf = ft_strdup(ft_keepreading(fd, &i, &readreturn));
+		*savedread = ft_tosaveornot(&buf, i, *savedread);
+	}
+	if (readstatus == 1)
+	{
+		i = ft_findchar(*savedread, '\n');
+		if (i == -1)
 		{
-			buf = ft_strdup(ft_keepreading(fd, &i));
+			buf = ft_strdup(ft_keepreading(fd, &i, &readreturn));
+			buf = ft_strjoin(*savedread, buf);
 			*savedread = ft_tosaveornot(&buf, i, *savedread);
-			return (buf);
 		}
-		if (readstatus == 1)
+		else
 		{
-			i = ft_findchar(*savedread, '\n');
-			if (i == -1)
-			{
-				buf = ft_strdup(ft_keepreading(fd, &i));
-				*savedread = ft_tosaveornot(&buf, i, *savedread);
-				return (buf);
-			}
-			else
-			{
-				buf = ft_strdup(*savedread);
-				buf = ft_strsub(buf, 0, i - 1);
-				*savedread = ft_strsub(*savedread, i, ft_strlen(*savedread));
-				return (buf);
-			}
+			buf = ft_strdup(*savedread);
+			buf = ft_strsub(buf, 0, i - 1);
+			*savedread = ft_strsub(*savedread, i, ft_strlen(*savedread));
 		}
 	}
+	return (buf);
 }
 
 int get_next_line(const int fd, char **line)
 {
 	static char *savedread;
-	int readstatus;	
+	int readstatus;
+	int readreturn;	
 	
 	if (!savedread)
 		readstatus = 0;
@@ -106,6 +111,10 @@ int get_next_line(const int fd, char **line)
 		printf("Ce qui a ete saved precedemment : %s\n", savedread);
 		readstatus = 1;
 	}
-	*line =	ft_read(fd, &savedread, readstatus);
-return (0);
+	*line =	ft_read(fd, &savedread, readstatus, &readreturn);
+	if (readreturn > 0 || ft_strlen(savedread) != 0)
+		return (1);
+	else if (readreturn == -1)
+		return (-1);
+	return (0);
 }
--- gnl/main.c
+++ gnl/main.c
@@ -4,15 +4,14 @@ int main(int ac, char **av)
 {
 	char *line = NULL;
 	int fd;
-	int i = 0;
+	int i = -1;
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
-		while (i <= 4)
+		while (i != 0)
 		{
-			get_next_line(fd, &line);
+			printf("%d\n", i = get_next_line(fd, &line));
 			printf("Ce que je recupere avant de relancer gnl : %s\n", line);
-			i++;
 		}
 	}
 return(0);
